25bdee98bef835e7934ae44dfa03604198cc170b
elalaouifaris <elalaouifaris@gmail.com>
2018-07-21 19:05:37 +0000

    Initial commit with nvim and git config files

---
+++ setup_neovim.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Install neovim
+apt-get install software-properties-common
+apt-add-repository ppa:neovim-ppa/stable
+apt-get update
+apt-get install neovim
+apt-get install python-dev python-pip python3-dev python3-pip
+
+update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+update-alternatives --config vi
+update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+update-alternatives --config vim
+update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+update-alternatives --config editor
+
+# Install vim-Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Clone neovim profile:
+git clone https://github.com/elalaouifaris/neovim_config.git
+
+# Make an alias to the github init.vim
+mkdir -p ~/.config/nvim/
+cp ./neovim_config/w_init.vim ~/.config/nvim/init.vim
