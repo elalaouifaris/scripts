5a3722dd2b4c342f7bc4ef01428a0052c86b79b2
elalaouifaris <elalaouifaris@gmail.com>
2018-07-21 21:12:13 +0000

    setup scripts for kaggle credentials and competition data moving

---
+++ move_kaggle_data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp -rf ~/.kaggle/competitions/** /storage/kaggle/competitions/
---
+++ setup_kaggle.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pip install kaggle
+
+# Set up folder and credentials
+mkdir -p ~/.kaggle
+cp kaggle.json ~/.kaggle/kaggle.json
